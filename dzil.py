ddca88a201e9fb89c7ed4c717f80f899de62e067
andrewrjones <andrewjones86@googlemail.com>
2011-12-04 21:38:13 +0000

    implement DzilTest. Same as DzilSmoke, but runs 'test' instead of 'smoke'

--- dzil.py
+++ dzil.py
@@ -4,6 +4,9 @@ from twisted.python import log
 from buildbot.status.results import SUCCESS, FAILURE, WARNINGS, SKIPPED
 from buildbot.steps.shell import Test
 
+class DzilTest(Test):
+    command=["dzil", "test"]
+
 class DzilSmoke(Test):
     command=["dzil", "smoke"]
 
